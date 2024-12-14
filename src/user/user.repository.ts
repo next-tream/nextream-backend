f85f715bfc0eb79272eb103340b21c3392e7c115
지승 <rjs8833@gmail.com>
2024-12-14 21:01:52 +0900

    add dockerignore

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './entity/user.entity';
